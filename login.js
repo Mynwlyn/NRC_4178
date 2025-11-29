4ef7852896800d269d81cad1a969c046e980d1ca
Anderson <calderonhuamanjuan@gmail.com>
2025-11-29 12:35:52 +0000

    commit

--- login.js
+++ login.js
@@ -4,17 +4,24 @@ function validarLogin(event) {
   event.preventDefault();
 
   // Clave estática para la demo
+  const CORREO = "miyaball@gmail";
+  const emailInput = document.getElementById("email").value;
   const CLAVE_SECRETA = "miyaball";
   const passwordInput = document.getElementById("password").value;
 
   // 2. Realizar la verificación
-  if (passwordInput.toLowerCase() === CLAVE_SECRETA) {
-    alert("¡Acceso concedido! Bienvenido.");
-    // Redirige a tu página principal
-    window.location.href = "LandingPage.html";
+  if (emailInput.toLowerCase() === CORREO) {
+    if (passwordInput.toLowerCase() === CLAVE_SECRETA) {
+      alert("¡Acceso concedido! Bienvenido.");
+      // Redirige a tu página principal
+      window.location.href = "LandingPage.html";
+    } else {
+      alert("Clave incorrecta. Inténtalo de nuevo.");
+      document.getElementById("password").value = "";
+    }
   } else {
-    alert("Clave incorrecta. Inténtalo de nuevo.");
-    document.getElementById("password").value = "";
+    alert("Correo no encontrado. Inténtelo con un correo existente.");
+    document.getElementById("Email").value = "";
   }
 
   return false;
